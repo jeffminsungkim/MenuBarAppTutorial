3a6e47a36b33fb270f09f953d0507166fd6e304f
jeffminsungkim <jeffminsungkim@gmail.com>
2020-08-29 16:14:58 +0900

    Initial Commit

---
+++ MenuBarAppTutorial/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  MenuBarAppTutorial
+//
+//  Created by jeffminsungkim on 2020/08/29.
+//  Copyright © 2020 jeffminsungkim. All rights reserved.
+//
+
+import Cocoa
+import SwiftUI
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+
+        // Create the window and set the content view. 
+        window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            backing: .buffered, defer: false)
+        window.center()
+        window.setFrameAutosaveName("Main Window")
+        window.contentView = NSHostingView(rootView: contentView)
+        window.makeKeyAndOrderFront(nil)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
